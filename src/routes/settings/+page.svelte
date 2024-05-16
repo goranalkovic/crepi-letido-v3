4682dc45986746b8df9cacafed0a95dc8c687ed5
Goran Alković <goranalkovic@users.noreply.github.com>
2024-05-16 06:32:00 +0200

    add avatar options

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -7,8 +7,10 @@
 	import { Button } from '$lib/components/ui/button';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import * as Avatar from '$lib/components/ui/avatar';
-	import DropdownMenuItem from '$lib/components/ui/dropdown-menu/dropdown-menu-item.svelte';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
+
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import SunMoon from 'lucide-svelte/icons/sun-moon';
 
 	let { data } = $props();
 	let { userData } = $state(data);
@@ -21,14 +23,21 @@
 		{ value: 'goc', label: 'Goc' },
 		{ value: 'hrvoic', label: 'Hrvoić' },
 		{ value: 'jurica', label: 'Jurica' },
+		{ value: 'jurica2', label: 'Jurica (2)' },
 		{ value: 'kanc', label: 'KancIvan' },
 		{ value: 'my10', label: 'Majcen' },
 		{ value: 'petarjadek', label: 'Petar' },
+		{ value: 'tamara', label: 'Tamara' },
 		{ value: 'ext1', label: 'Gost 1' },
 		{ value: 'ext2', label: 'Gost 2' },
 		{ value: 'ext3', label: 'Gost 3' },
 		{ value: 'ext4', label: 'Gost 4' },
 		{ value: 'ext5', label: 'Gost 5' },
+		{ value: 'ext6', label: 'Gost 6' },
+		{ value: 'ext7', label: 'Gost 7' },
+		{ value: 'ext8', label: 'Gost 8' },
+		{ value: 'ext9', label: 'Gost 9' },
+		{ value: 'ext10', label: 'Gost 10' },
 	];
 
 	const themeColorOptions = [
@@ -109,6 +118,16 @@
 									{label}
 								</DropdownMenu.RadioItem>
 							{/each}
+							<DropdownMenu.Separator />
+							{#each avatarOptions.filter(({ value }) => value.startsWith('ext')) as { label, value }}
+								<DropdownMenu.RadioItem {value}>
+									<Avatar.Root class="mr-2 size-8">
+										<Avatar.Image src="/profile-pictures/{value}.jpg" alt={label} />
+										<Avatar.Fallback>{value}</Avatar.Fallback>
+									</Avatar.Root>
+									{label}
+								</DropdownMenu.RadioItem>
+							{/each}
 						</DropdownMenu.RadioGroup>
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
@@ -116,7 +135,7 @@
 					<p transition:slide class="text-sm text-muted-foreground">E-mail is not valid!</p>
 				{/if} -->
 
-				<input type="hidden" name="avatar" value="{formData?.avatar}">
+				<input type="hidden" name="avatar" value={formData?.avatar} />
 			</div>
 		</Card.Content>
 	</Card.Root>
@@ -128,32 +147,35 @@
 		</Card.Header>
 		<Card.Content class="space-y-4">
 			<div class="space-y-1.5">
-				<Label>Sunce, mesec i tak</Label>
+				<Label>Sunce, mesec i tak (svjetlina sučelja)</Label>
 				<br />
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger asChild let:builder>
 						<Button class="justify-start w-full h-16 py-0" variant="outline" builders={[builder]}>
-							{formData?.themeMode}
+							{#if formData?.themeMode === 'light'}
+								<Sun class="mr-2 opacity-75 size-8" />
+								Vužgaj nek se troši
+							{:else if formData?.themeMode === 'dark'}
+								<Moon class="mr-2 opacity-75 size-8" />
+								Vampir mode
+							{:else}
+								<SunMoon class="mr-2 opacity-75 size-8" />
+								Kak šef odluči, tak bu (system)
+							{/if}
 						</Button>
 					</DropdownMenu.Trigger>
 					<DropdownMenu.Content class="w-56">
 						<DropdownMenu.Label>Kak pa da svetli?</DropdownMenu.Label>
 						<DropdownMenu.Separator />
 						<DropdownMenu.RadioGroup bind:value={formData.themeMode}>
-							<DropdownMenu.RadioItem value='system'>
-								Kak šef hoće (system)
-							</DropdownMenu.RadioItem>
-							<DropdownMenu.RadioItem value='light'>
-								Vužgaj svetlo navek (light)
-							</DropdownMenu.RadioItem>
-							<DropdownMenu.RadioItem value='dark'>
-								Vampir sam (dark)
-							</DropdownMenu.RadioItem>
+							<DropdownMenu.RadioItem value="system">Kak šef hoće (system)</DropdownMenu.RadioItem>
+							<DropdownMenu.RadioItem value="light">Vužgaj svetlo navek (light)</DropdownMenu.RadioItem>
+							<DropdownMenu.RadioItem value="dark">Vampir sam (dark)</DropdownMenu.RadioItem>
 						</DropdownMenu.RadioGroup>
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
 
-				<input type="hidden" name="theme-mode" value="{formData?.themeMode}">
+				<input type="hidden" name="theme-mode" value={formData?.themeMode} />
 			</div>
 
 			<div class="space-y-1.5">
@@ -177,14 +199,14 @@
 								</DropdownMenu.RadioItem>
 
 								{#if value === 'default'}
-									 <DropdownMenu.Separator />
+									<DropdownMenu.Separator />
 								{/if}
 							{/each}
 						</DropdownMenu.RadioGroup>
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
 
-				<input type="hidden" name="theme-color" value="{formData?.themeColor}">
+				<input type="hidden" name="theme-color" value={formData?.themeColor} />
 			</div>
 		</Card.Content>
 	</Card.Root>
