6348270d982bbf8f8966e27d541a3d27d661d0b8
Goran Alković <goranalkovic@users.noreply.github.com>
2024-05-16 23:53:20 +0200

    add very early implementation of voting system

---
+++ src/routes/gableci/vote/+page.server.js
@@ -0,0 +1,171 @@
+// @ts-nocheck
+import { getCurrentDate } from '$lib/current-date.js';
+
+export const load = async ({ depends, locals: { supabase, user } }) => {
+	const currentDate = getCurrentDate();
+
+	const { data: customRestaurantData, error: err1 } = await supabase.from('custom-meal-data').select('id,created,meals,restaurant:restaurants(*)');
+	const { data: restaurantData, error: err2 } = await supabase.from('meal-data').select('id,created,meals,restaurant:restaurants(*)').eq('valid', 'TRUE');
+	const { data: userData, error: err3 } = await supabase.from('users').select().eq('email', user?.email).maybeSingle();
+
+	const { data: existingData, error: err4 } = await supabase
+		.from('meal-selections')
+		.select('selected,final,user:users(*)')
+		.gte('created', `${currentDate} 00:00:00`)
+		.lte('created', `${currentDate} 23:59:59`);
+
+	let possibleSelections = [];
+
+	possibleSelections = restaurantData.reduce((prev, { meals, restaurant: { slug } }) => {
+		return {
+			...prev,
+			[slug]: [...Array(meals.length)].map((_, i) => ({ meal: i, selected: [] })),
+		};
+	}, {});
+
+	possibleSelections = customRestaurantData.reduce(
+		(prev, { meals, restaurant: { slug } }) => {
+			return {
+				...prev,
+				[slug]: [...Array(meals.length)].map((_, i) => ({ meal: i, selected: [] })),
+			};
+		},
+		{ ...possibleSelections }
+	);
+
+	let chosenRestaurants = [];
+
+	// const usersWithoutSelections = existingData.filter(({ selected }) => Object.keys(selected)?.length < 1);
+	const usersWithSelections = existingData.filter(({ selected }) => Object.keys(selected)?.length > 0);
+
+	existingData.forEach(({ selected, user }) => {
+		Object.entries(selected).forEach(([slug, meals]) => {
+			if (!chosenRestaurants.includes(slug)) {
+				chosenRestaurants.push(slug);
+			}
+
+			meals.forEach(({ meal }) => {
+				possibleSelections[slug][meal].selected.push(user);
+			});
+		});
+	});
+
+	const userStatisticsData = existingData
+		.filter(({ selected }) => Object.keys(selected)?.length > 0)
+		.map((i) => ({ mail: i.user.email, selections: i.selected }))
+		.map((i) => {
+			return {
+				...i,
+				numMealsOrdered: Object.entries(i.selections).reduce((prev, [k, v]) => ({
+					...prev,
+					[k]: v.length,
+				}), {}),
+			}
+		})
+		.map((i) => {
+			return {
+				...i,
+				totalMealsOrdered: Object.values(i.numMealsOrdered).reduce((prev, curr) => prev + parseInt(curr), 0),
+			}
+		});
+
+	let displayedRestaurants = [...restaurantData, ...customRestaurantData].filter(Boolean)
+		.filter(({ restaurant: { slug } }) => chosenRestaurants.includes(slug))
+		.map((i) => ({
+			...i,
+			statistics: {
+				hasIntersect: userStatisticsData.every((userData) => Object.keys(userData.numMealsOrdered).includes(i.restaurant.slug)),
+			}
+		}))
+		.filter((i) => i.statistics.hasIntersect)
+
+		.toSorted((a, b) => {
+			if (a.statistics.totalMealsOrdered === b.statistics.totalMealsOrdered) {
+				return 0;
+			}
+
+			return b.statistics.totalMealsOrdered - a.statistics.totalMealsOrdered;
+		})
+		.toSorted((a, b) => {
+			if (a.statistics.intersectBreakers?.length === b.statistics.intersectBreakers?.length) {
+				return 0;
+			}
+
+			return b.statistics.intersectBreakers?.length - a.statistics.intersectBreakers?.length;
+		})
+		.toSorted((a, b) => {
+			if (a.statistics.hasIntersect && b.statistics.hasIntersect) {
+				return 0;
+			}
+
+			if (a.statistics.hasIntersect) {
+				return -1;
+			}
+
+			return 1;
+		});
+
+	const finalize1DoneUsers = existingData.filter(({ final }) => final === true);
+
+	const { data: currentUserHasFinalized1 } = await supabase
+		.from('meal-selections')
+		.select('final,finalVote')
+		.eq('user', user?.email)
+		.gte('created', `${currentDate} 00:00:00`)
+		.lte('created', `${currentDate} 23:59:59`)
+		.maybeSingle();
+
+	const { data: allUserData } = await supabase.from('users').select();
+
+	const { data: allThatFinalizedStep2 } = await supabase
+		.from('meal-selections')
+		.select()
+		.not('finalVote', 'is', null)
+		.gte('created', `${currentDate} 00:00:00`)
+		.lte('created', `${currentDate} 23:59:59`);
+
+	const sortedAllThatFinalizedStep2 = allThatFinalizedStep2.toSorted((a, b) => {
+		if (a.user === user.email) {
+			return 1;
+		}
+
+		return -1;
+	}).map((item) => {
+		const parsedFinalData = JSON.parse(item.finalVote);
+
+		const moreData = [...restaurantData, ...customRestaurantData].filter(Boolean).reduce((prev, curr) => {
+			if (curr.restaurant.slug === parsedFinalData.rest) {
+				return {
+					...prev,
+					restaurant: curr.restaurant,
+					meal: curr.meals[parsedFinalData.meal],
+				};
+			}
+
+			return prev;
+		}, {});
+
+		return {
+			user: allUserData.find(({ email }) => email === item.user),
+			...moreData,
+		};
+	});
+
+	console.log(sortedAllThatFinalizedStep2);
+
+	return {
+		restaurants: restaurantData ?? [],
+		customRestaurants: customRestaurantData ?? [],
+		userData,
+		numUsers: existingData?.length,
+		possibleSelections,
+		chosenRestaurants,
+		displayedRestaurants,
+		numPeoplePicked: usersWithSelections?.length ?? 0,
+		hasError: err1 || err2 || err3 || err4,
+		finalize1DoneUsers,
+		currentUserHasFinalized1: currentUserHasFinalized1?.final,
+		currentUserHasFinalized2: currentUserHasFinalized1?.finalVote,
+		allStep2Finalists: sortedAllThatFinalizedStep2,
+	};
+};
--- src/routes/gableci/vote/+page.svelte
+++ src/routes/gableci/vote/+page.svelte
@@ -1,27 +1,228 @@
 <script>
-	import Vote from 'lucide-svelte/icons/vote';
+	// @ts-nocheck
+	import { invalidateAll } from '$app/navigation';
+	import Vegan from 'lucide-svelte/icons/vegan';
+	import Phone from 'lucide-svelte/icons/phone';
+	import Truck from 'lucide-svelte/icons/truck';
+	import BookMarked from 'lucide-svelte/icons/book-marked';
+	import Handshake from 'lucide-svelte/icons/handshake';
+	import CloudRainWind from 'lucide-svelte/icons/cloud-rain-wind';
+	import Sticker from 'lucide-svelte/icons/sticker';
+	import CircleCheckBig from 'lucide-svelte/icons/circle-check-big';
+	import PartyPopper from 'lucide-svelte/icons/party-popper';
+	import RouteOff from 'lucide-svelte/icons/route-off';
+	import Hourglass from 'lucide-svelte/icons/hourglass';
+	import Atom from 'lucide-svelte/icons/atom';
+	import TicketCheck from 'lucide-svelte/icons/ticket-check';
+
+	import { getCurrentDate } from '$lib/current-date.js';
+
+	import { slide, scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import * as Alert from '$lib/components/ui/alert';
+	import { Separator } from '$lib/components/ui/separator';
+	import * as HoverCard from '$lib/components/ui/hover-card';
+	import * as Popover from '$lib/components/ui/popover';
 
-	let buttonDisabled = $state(false);
+	let { data } = $props();
+	let { supabase, possibleSelections, hasError, displayedRestaurants, numPeoplePicked, user, allStep2Finalists, finalize1DoneUsers, currentUserHasFinalized1, currentUserHasFinalized2, numUsers } =
+		$derived(data);
+
+	$effect(() => {
+		const mealSelectionsChannel = supabase
+			.channel('resultsMealSelectionSub')
+			.on('postgres_changes', { event: '*', schema: 'public', table: 'meal-selections' }, async () => await invalidateAll())
+			.subscribe();
+
+		const restaurantDataChannel = supabase
+			.channel('resultsRestaurantDataSub')
+			.on('postgres_changes', { event: '*', schema: 'public', table: 'restaurants' }, async () => await invalidateAll())
+			.subscribe();
+
+		return () => {
+			supabase.removeChannel(mealSelectionsChannel);
+			supabase.removeChannel(restaurantDataChannel);
+		};
+	});
+
+	let selectedFinalMeal = $state();
+
+	const handleFinalize2 = async () => {
+		const currentDate = getCurrentDate();
+
+		await supabase
+			.from('meal-selections')
+			.update({
+				finalVote: selectedFinalMeal,
+			})
+			.eq('user', user.email)
+			.gte('created', `${currentDate} 00:00:00`)
+			.lte('created', `${currentDate} 23:59:59`);
+	};
+
+	$effect(() => {
+		invalidateAll();
+	});
 </script>
 
 <svelte:head>
-	<title>Final vote | ČL</title>
+	<title>Gablec overview | ČL</title>
 </svelte:head>
 
-<div class="w-full max-w-lg m-auto space-y-4">
-	<Vote class="mx-auto size-20 accent-current" />
+<Alert.Root class="max-w-sm">
+	<Alert.Title>Halo, halo, stani malo</Alert.Title>
+	<Alert.Description>Još nisam ful siguran kak ovo želim složiti, stvari se možda promijene u budućnosti.</Alert.Description>
+</Alert.Root>
 
-	<Card.Root>
-		<Card.Header>
-			<Card.Title tag="h2">Coming soon</Card.Title>
-			<Card.Description>(kad mi se bude dalo)</Card.Description>
+{#if finalize1DoneUsers?.length < numUsers}
+	<Card.Root class="w-full max-w-md m-auto text-center">
+		<Card.Header class="py-10">
+			<Hourglass class="mx-auto text-accent-foreground size-16 " />
+			<Card.Title class="text-2xl md:text-3xl">Strpljenje, molim</Card.Title>
+			<Card.Description class="text-lg">Još nisu svi finalizirali</Card.Description>
 		</Card.Header>
-		<Card.Content>Jbg.</Card.Content>
-		<Card.Footer>
-			<Button class="mx-auto" disabled={buttonDisabled} on:click={() => (buttonDisabled = true)} size="lg">Žali se upravi vodovoda</Button>
-		</Card.Footer>
 	</Card.Root>
-</div>
+{:else if !currentUserHasFinalized2}
+	{#each displayedRestaurants as { id, meals, restaurant: { name, phone, slug, url, delivery, urlType }, statistics: { intersectBreakers, hasIntersect } } (id)}
+		<div class="flex gap-8 pb-8 border-b last:border-b-0" transition:scale={{ start: 0.95 }} animate:flip={{ duration: 500 }}>
+			<Card.Root class="sticky self-start min-w-96 top-8">
+				<Card.Header>
+					<Card.Title class="flex items-center gap-2 mb-3">
+						<img class="size-6" src="/restaurant-icons/{slug}.png" alt={name} />
+						{name}
+					</Card.Title>
+					{#if phone}
+						<Card.Description class="flex items-center gap-3">
+							<Phone class="size-5 accent-current" />
+							{phone}
+						</Card.Description>
+					{/if}
+					{#if delivery}
+						<Card.Description class="flex items-center gap-3">
+							<Truck class="size-5 accent-current" />
+							{delivery}
+						</Card.Description>
+					{/if}
+
+					{#if url}
+						<Card.Description class="flex items-center gap-3 pt-3">
+							<BookMarked class="size-5 accent-current" />
+
+							<Button variant="link" class="h-auto p-0" href={url} target="_blank">
+								{#if urlType === 'additional'}
+									Dodatna ponuda
+								{:else}
+									Meni
+								{/if}
+							</Button>
+						</Card.Description>
+					{/if}
+				</Card.Header>
+			</Card.Root>
+
+			<div class="space-y-1.5 w-full max-w-lg p-1 rounded-lg">
+				{#each meals as { name, price, meta }, i (i)}
+					{#if possibleSelections[slug][i].selected?.findIndex(({ email }) => email === user?.email) > -1}
+						<div transition:slide class="flex items-center w-full p-2 space-x-3 transition duration-700 rounded-lg">
+							<div class="space-y-1.5 w-full text-sm font-medium leading-none mx-w-lg">
+								<p>
+									{name}
+									{#if meta?.isVegetarian}
+										<Vegan class="inline-block ml-1 size-4 text-lime-600 dark:text-lime-600" />
+									{/if}
+								</p>
+								{#if price}
+									<p class="text-muted-foreground">{price}</p>
+								{/if}
+							</div>
+
+							<Button
+								variant={selectedFinalMeal?.rest === slug && selectedFinalMeal?.meal === i ? 'default' : 'outline'}
+								on:click={() => {
+									selectedFinalMeal = {
+										rest: slug,
+										meal: i,
+									};
+								}}
+							>
+								Odaberi restoran i jelo
+							</Button>
+						</div>
+					{/if}
+				{/each}
+			</div>
+		</div>
+	{/each}
+{:else}
+	<Card.Root class="w-full max-w-md mx-auto my-10 text-center">
+		<Card.Header class="py-10">
+			<TicketCheck class="mx-auto text-accent-foreground size-16 " />
+			<Card.Title class="text-2xl md:text-3xl">To je to!</Card.Title>
+			<Card.Description class="text-lg">Ispod se budu pojavili glasovi drugih ljudi</Card.Description>
+		</Card.Header>
+	</Card.Root>
+
+	<div class="grid w-full max-w-screen-lg grid-cols-3 mx-auto text-lg font-bold">
+		<p>Korisnik</p>
+		<p>Restoran</p>
+		<p>Jelo</p>
+	</div>
+	{#each allStep2Finalists as { user: { firstName, lastName, avatar }, restaurant: { name, slug }, meal }}
+		<div class="grid w-full max-w-screen-lg grid-cols-3 mx-auto">
+			<div class="flex items-center gap-2">
+				<Avatar.Root class="hidden size-8 sm:flex">
+					<Avatar.Image src="/profile-pictures/{avatar}.jpg" alt="{firstName} {lastName}" />
+					<Avatar.Fallback>{firstName?.charAt(0) ?? '-'}{lastName?.charAt(0) ?? '-'}</Avatar.Fallback>
+				</Avatar.Root>
+				<p class="text-sm font-medium leading-none">{firstName} {lastName}</p>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<img class="size-6" src="/restaurant-icons/{slug}.png" alt={name} />
+				{name}
+			</div>
+			<div class="space-y-1.5 w-full text-sm font-medium leading-none mx-w-lg">
+				<p>
+					{meal.name}
+				</p>
+				{#if meal.price}
+					<p class="text-muted-foreground">{meal.price}</p>
+				{/if}
+			</div>
+		</div>
+	{/each}
+{/if}
+
+{#if !currentUserHasFinalized2 && selectedFinalMeal}
+	<div transition:scale={{ start: 0.85 }} class="fixed z-20 w-80 right-10 bottom-10">
+		<Card.Root class="shadow-lg">
+			<Card.Header>
+				<Card.Title class="inline-flex items-center gap-2">
+					<Atom class="stroke-[1.5] size-7" />
+					Nisam Tarik, ali...
+				</Card.Title>
+				<Card.Description class="text-balance">
+					Je li ovo tvoj konačni odgovor?
+					<br />
+					<br />
+					Ako je, <strong>finaliziraj</strong> i onda čekaš!
+				</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<Popover.Root>
+					<Popover.Trigger><Button>Finaliziraj</Button></Popover.Trigger>
+					<Popover.Content class="flex flex-col gap-2 shadow-2xl w-80">
+						<span>There's no going back!<br />Valjda je to sve</span>
+						<span class="mb-3 text-xs text-muted-foreground">Naknadno otključavanje: 5€ + PDV <span class="select-none opacity-15">(nije lol)</span></span>
+						<Button on:click={() => handleFinalize2()} variant="destructive" class="ml-auto">Ideme!</Button>
+					</Popover.Content>
+				</Popover.Root>
+			</Card.Content>
+		</Card.Root>
+	</div>
+{/if}
